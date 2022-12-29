b536f3075443a831aec03a5b1d7f5c59e3796f2d
HellWayer <110084751+HellWayer@users.noreply.github.com>
2022-12-29 22:45:48 +0700

    Update Paid.lua

--- Paid.lua
+++ Paid.lua
@@ -1,4 +1,8 @@
 local placeId = game.PlaceId
+getgenv().Key = "IT_LOCKER_HUB_8987_GIRO_6975_MFIF"
+Id = "1056892681600634921"  
+token = "LwhofAJMuwygWshJr2hrfy7bXIrTqog9fQqNepVTD8cBbIHKs6iu2FgtDxLoBsA7LRY9"  
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Hellwey/main/api.lua", true))()
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Locker-Hub/main/BF.lua"))()
 elseif placeId == 2788229376 then
