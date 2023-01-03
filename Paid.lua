de6cd3220d5002a385f6ada69cd974e60a6438d5
HellWayer <110084751+HellWayer@users.noreply.github.com>
2023-01-03 22:57:44 +0700

    Update Paid.lua

--- Paid.lua
+++ Paid.lua
@@ -1,7 +1,7 @@
 local placeId = game.PlaceId
 Key = "IT_LOCKER_HUB";
-Id = "1059860084768899102";
-token = "qYAsda2QgYCMNIeN3MLHHUtUfjFFjGfh2gQhlQJwwAbIANnH2wrE51HEsY3b3lw1iD5X";
+Id = "1056892681600634921";
+token = "LwhofAJMuwygWshJr2hrfy7bXIrTqog9fQqNepVTD8cBbIHKs6iu2FgtDxLoBsA7LRY9";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Whilelist-Checker/main/Verifyer.lua", true))()
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Locker-Hub/main/BF.lua"))()
