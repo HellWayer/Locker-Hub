933412c23bfdd5313e3dcd986ae78a392fbf3ae1
HellWayer <110084751+HellWayer@users.noreply.github.com>
2023-01-09 22:58:18 +0700

    Update Paid.lua

--- Paid.lua
+++ Paid.lua
@@ -3,6 +3,7 @@ Key = "IT_LOCKER_HUB";
 Id = "1056892681600634921";
 token = "LwhofAJMuwygWshJr2hrfy7bXIrTqog9fQqNepVTD8cBbIHKs6iu2FgtDxLoBsA7LRY9";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Whilelist-Checker/main/Verifyer.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Locker-Hub/main/IT-System.lua"))()
 if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HellWayer/Locker-Hub/main/BF.lua"))()
     game:GetService("ReplicatedStorage").Util.Sound.Storage.Ope.RadioKnife.SwordSwing:Destroy()
